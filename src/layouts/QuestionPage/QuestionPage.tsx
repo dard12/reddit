49a13530de2a5cd62a6bec4cb129bd1b92180dd0
Ricky Lung <dard12@gmail.com>
2019-10-18 21:12:42 -0400

    remove extra import

--- src/layouts/QuestionPage/QuestionPage.tsx
+++ src/layouts/QuestionPage/QuestionPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import styles from './QuestionPage.module.scss';
