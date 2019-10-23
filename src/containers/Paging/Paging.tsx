9edeecd75866ea28e8e66f40300fadf00ab0f24a
Ricky Lung <dard12@gmail.com>
2019-10-23 00:05:27 -0400

    set paging in query params

--- src/containers/Paging/Paging.tsx
+++ src/containers/Paging/Paging.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
 import styles from './Paging.module.scss';
+import { getQueryParams, setQueryParams } from '../../history';
 
 interface PagingProps {
   params: any;
@@ -9,8 +10,15 @@ interface PagingProps {
 }
 
 function Paging(props: PagingProps) {
-  const { params, PageComponent, className = styles.pageGrid } = props;
-  const [page, setPage] = useState(0);
+  const { params = {}, PageComponent, className = styles.pageGrid } = props;
+  const initialPage = _.toNumber(getQueryParams('page')) || 0;
+  const [page, setPage] = useState(initialPage);
+
+  const seeMore = () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+    setQueryParams({ page: nextPage });
+  };
 
   return (
     <div className={className}>
@@ -18,7 +26,7 @@ function Paging(props: PagingProps) {
         <PageComponent
           key={currPage}
           params={{ ...params, page: currPage }}
-          seeMore={currPage === page ? () => setPage(page + 1) : undefined}
+          seeMore={currPage === page ? seeMore : undefined}
         />
       ))}
     </div>
