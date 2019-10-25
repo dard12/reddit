ec996976b8e3bd8440fcfd437204046e423f48b6
Ricky Lung <dard12@gmail.com>
2019-10-25 17:53:04 -0400

    remove tag tabs for now

--- src/layouts/Questions/Questions.tsx
+++ src/layouts/Questions/Questions.tsx
@@ -10,24 +10,14 @@ interface QuestionsProps {}
 
 function Questions(props: QuestionsProps) {
   const query = getQueryParams('query');
-  const tag = getQueryParams('tag');
   const params = {
-    search: { text: query, tags: [tag] },
+    search: { text: query },
     sort: 'up_vote',
   };
-
-  const tabs = [
-    { label: 'All', value: 'all' },
-    { label: 'Technical Skill', value: 'technical' },
-    { label: 'Team Fit', value: 'fit' },
-    { label: 'Personal Motivation', value: 'motivation' },
-    { label: 'Fun / Other', value: 'fun' },
-  ];
 
   return (
     <div className={styles.questionsPage}>
       <SearchBar query={query} />
-      <Tabs tabs={tabs} queryParamName="tag" initialTab="all" />
       <Paging component={QuestionListPage} params={params} />
     </div>
   );
